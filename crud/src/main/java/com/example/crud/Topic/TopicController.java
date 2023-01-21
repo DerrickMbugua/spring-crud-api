2be1c2084f08592d50ca047ac2e082be81919100
“derrick” <derrickmbugua50@gmail.com>
2023-01-21 09:11:56 +0300

    feat: db connection

--- crud/src/main/java/com/example/crud/Topic/TopicController.java
+++ crud/src/main/java/com/example/crud/Topic/TopicController.java
@@ -10,12 +10,14 @@ import java.util.List;
 @RequestMapping("/api/v1")
 public class TopicController {
 
+    private final TopicService topicService;
+
+    public TopicController(TopicService topicService) {
+        this.topicService = topicService;
+    }
+
     @GetMapping("/topics")
     public List<Topic> getTopics(){
-        return List.of(
-          new Topic("Wolves", "Wolves Description"),
-                new Topic("Wolves", "Wolves Description")
-
-                );
+        return topicService.getTopics();
     }
 }
--- crud/src/main/java/com/example/crud/Topic/TopicService.java
+++ crud/src/main/java/com/example/crud/Topic/TopicService.java
@@ -1,4 +1,16 @@
 package com.example.crud.Topic;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TopicService {
+    public List<Topic> getTopics(){
+        return List.of(
+                new Topic("Wolves", "Wolves Description"),
+                new Topic("Wolves", "Wolves Description")
+
+        );
+    }
 }
